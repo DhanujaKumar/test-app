24e4cc3ef45f781608bb6647bda8a9f77fe20e9a
DhanujaKumar <vdhanujakumar@gmail.com>
2025-06-09 14:11:20 +0530

    with css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 
-
 export default function Home() {
   return (
     
-  <>
+  <div class="main">
   <h1>Welcome to GovEase</h1>
   <p>Designed to simplify and streamline the process of obtaining government approvals, licenses, and permits.</p>
-  <button>Get Started</button>
-  </>
+  <button class="button">Get Started</button>
+  </div>
   );
 }
