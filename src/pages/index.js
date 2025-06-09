d81f93d93721351d808c5cb7d5016ad00b3d8024
DhanujaKumar <vdhanujakumar@gmail.com>
2025-06-09 13:50:29 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    
+  <>
+  <h1>Welcome to GovEase</h1>
+  <p>Designed to simplify and streamline the process of obtaining government approvals, licenses, and permits.</p>
+  <button>Get Started</button>
+  </>
+  );
+}
